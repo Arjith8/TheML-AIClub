038c5fff03aa3acaf0aaeafd7b45ebb283fc79d8
Arjithar8 <122336800+Arjith8@users.noreply.github.com>
2026-07-31 12:21:49 +0530

    dataset.py was buggy

--- xor_net/dataset.py
+++ xor_net/dataset.py
@@ -1,20 +1,9 @@
 import torch
 
-def create_dataset(input_dim: int = 2):
-    assert input_dim > 1
-    a: list[list[int]] = []
-    b: list[int] = []
-
-    for i in range(2**input_dim):
-        temp: list[int] = []
-        for j in range(input_dim):
-            temp.append((i >> j) & 1)
-        a.append(temp)
-
-    for i in a:
-        b.append(sum(i)%input_dim)
-
-
-    x = torch.tensor(a, dtype=torch.float)
-    y = torch.tensor(b, dtype=torch.float).unsqueeze(1)
+def create_dataset(
+    samples: int = 10000,
+    input_dim: int = 2,
+):
+    x = torch.randint(0, 2, (samples, input_dim)).float()
+    y = (x.sum(dim=1) % 2).float().unsqueeze(1)
     return x, y
