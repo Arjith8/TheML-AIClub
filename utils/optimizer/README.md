2eee44497861aa047ab184c210b7c11f24961e01
Arjithar8 <122336800+Arjith8@users.noreply.github.com>
2026-08-06 12:25:22 +0530

    Adam W implementation

--- utils/optimizer/README.md
+++ utils/optimizer/README.md
@@ -4,4 +4,9 @@
 - I referred https://arxiv.org/pdf/1412.6980 for ADAM specifically to understand the algo itself and why bias correction was used
 - They mention about a slight optimization on the compute but I think atleast for sake of code it makes the math a bit less intuitive in the sense I had to use pen and paper to understand it so not updating algo based on it
 - Overally I only first 3 pages, they were the ones I was looking forward to reading about the rest I skipped
+
+# ADAMW
+- https://arxiv.org/pdf/1711.05101 I only read till the proporsition part ie 3-early 4th page
+- Would this paper have existed if libs implemented actual weight decay one instead of l2 like one idk I still think there is so much I dont understand about this
+- Mannn paper reading is interesting and hard... Dang....
     
---
+++ utils/optimizer/adam_w.py
@@ -0,0 +1,41 @@
+from typing import override
+
+import torch
+
+from utils.optimizer.base import Optimizer
+
+
+class Adam(Optimizer):
+    def __init__(self, learning_rate: float, params: list[torch.Tensor], h_beta: float, m_beta: float, weight_decay: float) -> None:
+        self.learning_rate: float = learning_rate
+        self.params: list[torch.Tensor] = params
+        self.momentum: list[torch.Tensor] = [torch.zeros_like(i) for i in self.params]
+        self.history: list[torch.Tensor] = [torch.zeros_like(i) for i in self.params]
+        self.m_beta: float = m_beta
+        self.h_beta: float = h_beta
+        self.t: int = 0
+        self.weight_decay: float = weight_decay
+
+    @override
+    def step_zero_grad(self) -> None:
+
+        self.t += 1
+        for i, param in enumerate(self.params):
+            assert param.grad is not None
+            grad = param.grad
+
+            momentum = self.m_beta * self.momentum[i] + (1 - self.m_beta) * grad
+            momentum_bias_corrected = momentum / (1 - self.m_beta ** self.t)
+
+            history = self.h_beta * self.history[i] + (1 - self.h_beta) * torch.square(grad)
+            history_bias_corrected = history / (1 - self.h_beta ** self.t)
+
+            eps = torch.finfo(history.dtype).eps
+
+            adjusted_lr = self.learning_rate / (torch.sqrt(history_bias_corrected) + eps)
+            param -= adjusted_lr * momentum_bias_corrected
+            param -= self.learning_rate * self.weight_decay * param
+
+            self.momentum[i] = momentum
+            self.history[i] = history
+            _ = grad.zero_()
