5ce01594b4851f5f156bd48921df00b5e8fe6907
Arjithar8 <122336800+Arjith8@users.noreply.github.com>
2026-08-06 11:42:49 +0530

    auto grad only the implementation for Value ** Value, log and const ** Value should be left from what I can infer

--- autograd/data_structures.py
+++ autograd/data_structures.py
@@ -1,10 +1,9 @@
 # c7a9c53: Mmmm I am not as close to solution as I thought I will be I think I need a few more days, I have decided on the base
 # data structure but the backwards method I need to think more about it I will need to know what the operators are too so ig thats the next step say but backwards how will i implement it....
-from collections import deque
 from typing import Literal
 
 
-type Operation = Literal["+", "*"] | None
+type Operation = Literal["+", "*", "**"] | None
 class Value:
     def __init__(self, val: float, parents: list[Value] | None = None, operation: Operation = None):
         self.val: float = val
@@ -29,9 +28,33 @@ class Value:
     def __rmul__(self, other: int | float) -> Value:
         return self * other
 
+    def __sub__(self, other: Value | int | float) -> Value:
+        if not isinstance(other, Value):
+            other = Value(other)
+        other = -1 * other
+        return Value(val = self.val + other.val, parents=[self, other], operation="+")
+
+    def __rsub__(self, other: int | float) -> Value:
+        _other = Value(other)
+        return _other - self
+
+    def __pow__(self, other: Value | int | float) -> Value:
+        if isinstance(other, Value):
+            raise NotImplementedError("Value ** Value not implemented btw, needs quite a bit of thought")
+        return Value(val = self.val ** other, parents=[self, Value(other)], operation='**')
+
+    def __truediv__(self, other: Value | int | float) -> Value:
+        if not isinstance(other, Value):
+            other = Value(other)
+        
+        other = other ** -1
+        return self * other
+
+    def __rtruediv__(self, other: int | float) -> Value:
+        return Value(other) / self
+
     def backwards(self):
-        if not self.gradient:
-            self.gradient = 1.0
+        self.gradient = 1.0
 
         topologically_sorted_graph: list[Value] = []
         stack: list[tuple[Value, bool]] = []
@@ -58,5 +81,8 @@ class Value:
                 case "*":
                     parents[0].gradient += current.gradient * parents[1].val
                     parents[1].gradient += current.gradient * parents[0].val
+
+                case "**":
+                    parents[0].gradient = current.gradient * (parents[1].val) * parents[0].val ** (parents[1].val - 1)
                 case _:
                     pass
