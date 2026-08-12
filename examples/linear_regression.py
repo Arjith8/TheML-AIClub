a38a7e840c1ea75cb9c5d28f1f25168958ee8afc
Arjithar8 <122336800+Arjith8@users.noreply.github.com>
2026-08-12 16:57:34 +0530

    implemented batching

--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -1,3 +1,5 @@
+import torch
+
 from examples.data_mocks.regression_tasks import create_regression_dataset
 from nn.container.sequencial import Sequential
 from nn.layers.linear import Linear
@@ -14,12 +16,24 @@ def main():
     
     optimizer = Momentum(learning_rate=0.01, beta=0.9, params=model.parameters)
     epochs = 1000
-    for _ in range(epochs):
-        y_pred = model(data.X)
-        loss = MSE(data.y, y_pred)
-        loss.backward()                         # pyright: ignore[reportUnusedCallResult, reportUnknownMemberType]
+    batch_size = 100
+    data_size = data.X.size()[0]
+    total_batch_iters = (data_size + batch_size - 1) // batch_size
+    for epoch in range(epochs):
+        rand_perm = torch.randperm(data_size)
+        x = data.X[rand_perm]
+        y = data.y[rand_perm]
+        for batch in range(total_batch_iters):
+            batch_start_idx = batch*batch_size
+            x_batch = x[batch_start_idx: batch_start_idx+100]
+            y_batch = y[batch_start_idx: batch_start_idx+100]
+            y_pred = model(x_batch)
+            loss = MSE(y_batch, y_pred)
+            loss.backward()                         # pyright: ignore[reportUnusedCallResult, reportUnknownMemberType]
+            if epoch % 100 == 0:
+                print(epoch, loss.item())
 
-        optimizer.step_zero_grad()
+            optimizer.step_zero_grad()
 
     print(model.parameters, data.weights, data.bias, sep="\n")
 
